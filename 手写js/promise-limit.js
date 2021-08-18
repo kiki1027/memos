bd6140aabd27d8837dcd7aad9acae72ab1a9da3e
Wenkii <wuuuwq_1027@outlook.com>
2021-08-19 05:46:28 +0800

    手写js/封装一个并发请求控制函数

---
+++ 手写js/promise-limit.js
@@ -0,0 +1,121 @@
+// 写一个控制并发的函数，接收并发量参数
+/*
+思路：
+接收并发量参数 concurrency
+当前占用并发量 currentCount
+存储待执行任务队列 todoQueue
+
+面向对象思想：主谓 => 谁 干了什么
+谁：任务
+干了什么：并发判断 -> 任务执行
+*/
+class PromiseQueue {
+  // 初始化
+  constructor(options = {}) {
+    this.concurrency = options.concurrency || 1;
+    this.currentCount = 0;
+    this.todoQueue = [];
+  }
+
+  add(task) {
+    // 插入待执行队列
+    this.push(task);
+    // 执行任务
+    this.pop();
+  }
+
+  push(task) {
+    this.todoQueue.push(task);
+  }
+
+  pop() {
+    if (this.todoQueue.length === 0 || this.currentCount === this.concurrency) {
+      return;
+    }
+    // 并发量判断
+    var { task: todoTask } = this.todoQueue
+      .sort((a, b) => b.priority - a.priority)
+      .shift();
+    // 是否存在待执行任务
+    // 当前占用并发量+1
+    this.currentCount++;
+    // console.log(this.currentCount);
+    var promise = todoTask();
+    promise.then(this.finish.bind(this)).catch(this.finish.bind(this));
+  }
+
+  finish() {
+    // 继续执行下一个任务
+    this.currentCount--;
+    // 为了展现出执行间隔的效果，这里加一个延时作区分
+    this.pop();
+  }
+}
+
+var queue = new PromiseQueue({ concurrency: 3 });
+
+var promiseCreator = function (options = {}) {
+  return new Promise((resolve, reject) => {
+    console.log(options.id + " start ");
+    setTimeout(function () {
+      console.log(options.id + " OK, priority " + options.priority);
+      resolve();
+    }, options.time);
+  });
+};
+
+var options = [
+  {
+    id: 1,
+    time: 300,
+    priority: 3,
+  },
+  {
+    id: 2,
+    time: 300,
+    priority: 2,
+  },
+  {
+    id: 3,
+    time: 300,
+    priority: 1,
+  },
+  {
+    id: 4,
+    time: 300,
+    priority: 5,
+  },
+  {
+    id: 5,
+    time: 300,
+    priority: 2,
+  },
+  {
+    id: 6,
+    time: 300,
+    priority: 1,
+  },
+  {
+    id: 7,
+    time: 300,
+    priority: 6,
+  },
+  {
+    id: 8,
+    time: 300,
+    priority: 1,
+  },
+];
+
+var formatTask = function (option) {
+  return {
+    task: () => promiseCreator(option),
+    priority: option.priority,
+  };
+};
+
+options.forEach((option, i) => {
+  queue.add(formatTask(option));
+});
+
+queue.add(formatTask({ id: "first class", time: 100, priority: 10 }));
