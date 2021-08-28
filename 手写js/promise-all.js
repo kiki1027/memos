d0c5fb5adddcb81f5479b0d06843bde536d4561b
Wenkii <wuuuwq_1027@outlook.com>
2021-08-29 05:48:24 +0800

    手写js/promise.all

---
+++ 手写js/promise-all.js
@@ -0,0 +1,28 @@
+/**
+ * 手写 Promise.all
+ * 1. 接收一组promise
+ * 2. 并发执行
+ * 3. 返回一个promise
+ * 4. 遇到error后立即返回，reject promise
+ * 5. 无error且全部执行完成后返回一组结果，resolve promise
+ * @returns
+ */
+function myAll(promiseList) {
+  return new Promise((resolve, reject) => {
+    const resultList = []
+    promiseList.forEach((promise) => {
+      promise.then(
+        (val) => {
+          resultList.push(val)
+        },
+        (error) => {
+          reject(error)
+        }
+      )
+    })
+    // all fulfilled 返回
+    if (promiseList.length === resultList.length) {
+      resolve(resultList)
+    }
+  })
+}
