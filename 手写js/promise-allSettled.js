a2606f63343fc12ec6d2f3e3fadfa4b3d79a42ed
Wenkii <wuuuwq_1027@outlook.com>
2021-08-29 06:02:45 +0800

    手写js/promise.allSettled

---
+++ 手写js/promise-allSettled.js
@@ -0,0 +1,29 @@
+/**
+ * 手写Promise.allSettled
+ * 和Promise.all大致都差不多，但是返回值是一个组结果对象集（包括error）
+ * @returns
+ */
+const STATUS_FULFILLED = 'fulfilled'
+const STATUS_REJECTED = 'rejected'
+
+function myAllSettled(promiseList) {
+  return new Promise((resolve, reject) => {
+    const resultList = []
+    if (resultList.length > 0) {
+      resultList.forEach((promise) => {
+        Promise.resolve(promise).then(
+          (val) => {
+            resultList.push({ status: STATUS_FULFILLED, value: val })
+          },
+          (err) => {
+            resultList.push({ status: STATUS_REJECTED, reason: err })
+          }
+        )
+      })
+    }
+
+    if (resultList.length === promiseList.length) {
+      return resolve(resultList)
+    }
+  })
+}
